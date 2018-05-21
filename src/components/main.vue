44f03c2b615eebe8860c772a1e946864305993a5
zhushenshi <zhushenshi@126.com>
2018-05-21 18:09:04 +0800

    msg4

--- src/components/main.vue
+++ src/components/main.vue
@@ -2,6 +2,11 @@
   <div class="hello">
     <h1>{{ msg }}</h1>    
     <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1><h1>{{ msg }}</h1>
 
   </div>
 </template>

1afeda950aa416fa3a171b8b1bb2a020fcfc9d0e
zhushenshi <36190597+zhushenshi@users.noreply.github.com>
2018-05-27 18:34:57 +0800

    contact

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 
 import('./assets/css/base.css')
-
+import('./assets/css/mycss.css')
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

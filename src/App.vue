d2a347b212bd50bbd9fb9319d35071e714c58165
zhushenshi <zhushenshi@126.com>
2018-05-28 18:20:59 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
+  /* -webkit-font-smoothing: antialiased; */
+  /* -moz-osx-font-smoothing: grayscale; */
   color: #2c3e50;
 }
 </style>

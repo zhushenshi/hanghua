37462150fcfb052112c3c29e1e4ab3155d7cac7b
zhushenshi <zhushenshi@126.com>
2018-05-28 18:21:27 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,6 @@ export default {
   /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   /* -webkit-font-smoothing: antialiased; */
   /* -moz-osx-font-smoothing: grayscale; */
-  color: #2c3e50;
+  /* color: #2c3e50; */
 }
 </style>

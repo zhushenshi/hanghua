23a4ea70073c82f220d2cd72c1873f2c223ae2c1
zhushenshi <zhushenshi@126.com>
2018-05-29 18:09:47 +0800

    banner

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
     <header-view></header-view>
-    <h1>{{ msg }}</h1>    
+    <div class="banner">
+    <!-- 图片列表-->
+      <ul>
+        <li style="background: url(images/banner_01.jpg) center 0 no-repeat; display: block;z-index: 20;"><a href="#" class="link"></a></li>
+        <li style="background: url(images/banner_02.jpg) center 0 no-repeat;"><a href="#" class="link"></a></li>
+        <li style="background: url(images/banner_03.jpg) center 0 no-repeat;"><a href="#" class="link"></a></li>
+        <li style="background: url(images/banner_04.jpg) center 0 no-repeat;"><a href="#" class="link"></a></li>
+      </ul>
+      <!-- 左右箭头-->
+      <span class="cut prev"></span>
+      <span class="cut next"></span>
+      <!-- 小圆点指示器-->
+      <div class="indicator">
+        <a href="" class="cur"></a><a href=""></a><a href=""></a><a href=""></a>
+      </div>
+    </div>
+    <footer-view></footer-view>    
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import HeaderView from '@/components/Header.vue'
+import FooterView from '@/components/Footer.vue'
 
 export default {
   name:'Index',
@@ -16,7 +33,8 @@ export default {
     }
   },
   components: {
-    HeaderView
+    HeaderView,
+    FooterView
   }
 }
 </script>

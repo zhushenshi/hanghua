f68c693534ec42240d10739e757e5bdae462de1d
zhushenshi <zhushenshi@126.com>
2018-06-20 11:27:34 +0800

    swiper改写banner

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
     <header-view></header-view>
-    <div class="banner" @mouseenter="stop()" @mouseleave="play()">
-      <!-- 图片列表-->
-    
+    <!-- <div class="banner" @mouseenter="stop()" @mouseleave="play()">    
       <ul>
         <transition  name="fade">
         <li v-for="(img,index) in imgUrls" :key='index' v-if="index==mark"><a href="#" class="link"><img :src="img.src"></a></li>
         </transition>
       </ul>
-      <!-- 左右箭头-->
       <span class="cut prev" @click="change(-1)"></span>
       <span class="cut next" @click="change(1)"></span>
-      <!-- 小圆点指示器-->
       <div class="indicator">
         <a href="" v-for="(img,index) in imgUrls" :key='index' :class="{cur:index==mark}" @mouseenter='indicator(index)'></a>
       </div>
-    </div>
+    </div> -->
           <div class="swiper-container">
               <div class="swiper-wrapper">
-                  <div class="swiper-slide">Slide 1</div>
-                  <div class="swiper-slide">Slide 2</div>
-                  <div class="swiper-slide">Slide 3</div>
+                  <div class="swiper-slide" v-for="(img,index) in imgUrls" :key='index'><img :src="img.src" alt=""></div>
               </div>
               <!-- 如果需要分页器 -->
               <div class="swiper-pagination"></div>
@@ -77,12 +71,9 @@ export default {
         {src:require('../assets/images/banner_02.jpg')},
         {src:require('../assets/images/banner_03.jpg')},
         {src:require('../assets/images/banner_04.jpg')}
-        ],
-        mark:0,
-        timer: null
+        ]
     }
   },
-  created (){this.play();},
   mounted (){
   var mySwiper = new Swiper ('.swiper-container', {
     
@@ -105,36 +96,6 @@ export default {
   })      
   },
   methods: {
-   autoPlay () {  
-      this.mark++;  
-      if (this.mark === 4) { //当遍历到最后一张图片置零  
-        this.mark = 0  
-      }  
-    },
-    play(){
-       this.timer=setInterval(this.autoPlay, 2500)  
-      console.log(this.timer)
-       
-    },
-    stop(){
-      let That=this
-      clearTimeout(That.timer);
-      console.log('mouseenter')
-      console.log(this.timer)
-    },
-    change(parms){
-     if (this.mark === 3&&parms===1) {
-        this.mark = 0  
-        return
-      }else if(this.mark===0&&parms===-1){
-        this.mark = 3;
-        return
-      }
-      this.mark+=parms;  
-    },
-    indicator(index){
-      this.mark=index;
-    }
   },
   components: {
     HeaderView,
@@ -145,76 +106,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-	.banner {
-    height: 500px;
-    position: relative;
-    border-bottom: 3px solid #ddd;
-}
-.banner ul li {
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    display: block;
-    z-index: 10;
-    overflow:hidden;
-    transition: all 1s linear;
-}
-.banner .cut {
-    position: absolute;
-    width: 43px;
-    height: 75px;
-    opacity: 0.3;
-    top:50%;
-    margin-top: -38px;
-    left:50%;
-    background: #000 url(../assets/images/iconlist.png) no-repeat;
-    display: none;
-    z-index: 20;
-}
-.banner:hover .cut {
-    display: block;
-}
-.banner .cut:hover {
-    opacity: 0.5;
-    cursor: pointer;
-}
-.banner .prev {
-    margin-left:-485px;
-    background-position: 10px -325px;
-}
-.banner .next {
-    margin-left:442px;
-    background-position: 10px -395px;
-}
-.banner .indicator {
-    position: absolute;
-    width: 100%;
-    left:0;
-    bottom:24px;
-    text-align: center;
-    z-index: 20;
-}
-.banner .indicator a {
-    display: inline-block;
-    width: 14px;
-    height: 14px;
-    border-radius: 50%;
-    background: #ccc;
-    margin: 5px;
-}
-.banner .indicator a.cur {
-    background: #f90;
-}
-
-
 .swiper-container {
     width: 100%;
-    height: 300px;
 }  
 </style>

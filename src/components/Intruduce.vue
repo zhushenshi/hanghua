522e41a1d4eb45af8c7c8e20addb296730d6d0ff
zhushenshi <36190597+zhushenshi@users.noreply.github.com>
2018-06-29 16:49:28 +0800

    intro

---
+++ src/components/Intruduce.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+import HeaderView from '@/components/Header.vue'
+import FooterView from '@/components/Footer.vue'
+
+export default {
+  name:'About',
+  data () {
+    return {
+      msg: '',
+    }
+  },
+  created (){},
+  methods: {
+    
+  },
+  components: {
+    HeaderView,
+    FooterView
+  }
+}
+</script>
+<style scoped>
+</style>
+
